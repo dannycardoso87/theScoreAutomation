7a9ab668d8b23d83c12f67b017c78ddb7cfd31eb
dannycardoso87 <danny.cardoso87@gmail.com>
2024-05-09 02:26:49 -0400

    final improvements

--- src/test/kotlin/sportsapp/SportsAppTest.kt
+++ src/test/kotlin/sportsapp/SportsAppTest.kt
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.Test
 import sportsapp.pages.*
 
 class SportsAppTest : BaseTest() {
-
     @Test
     fun testAutomationFlow() {
         val getStartedPage = GetStartedPage(driver)
@@ -12,6 +11,15 @@ class SportsAppTest : BaseTest() {
         val mainPage = MainPage(driver)
         val teamPage = TeamPage(driver)
 
+        performOnboardingFlow(getStartedPage, favoriteLeaguePage, basePage, favoriteTeamPage)
+        navigateToTeamPage(mainPage, basePage)
+        verifyTeamPage(teamPage)
+        navigateToSubTabPage(teamPage)
+        verifyToSubTabPage(teamPage)
+        navigateBackToMainPage(basePage, mainPage)
+    }
+    private fun performOnboardingFlow(getStartedPage: GetStartedPage, favoriteLeaguePage: FavoriteLeaguePage, basePage: BasePage, favoriteTeamPage: FavoriteTeamPage,
+    ) {
         getStartedPage.clickGetStartedButton()
         favoriteLeaguePage.clickFavoriteLeagueButton("MLB Baseball")
         basePage.clickContinueButton()
@@ -22,13 +30,23 @@ class SportsAppTest : BaseTest() {
         basePage.clickMaybeLaterButton()
         basePage.clickAllowNotificationButton()
         basePage.clickOkGotItButton()
+    }
+    private fun navigateToTeamPage(mainPage: MainPage,basePage: BasePage) {
         mainPage.clickTeamIcon("TOR")
         basePage.clickCloseModalButton()
+    }
+    private fun verifyTeamPage(teamPage: TeamPage) {
         teamPage.verifyPageTitle("Toronto Blue Jays")
+    }
+    private fun navigateToSubTabPage(teamPage: TeamPage) {
         teamPage.clickTab("TEAM STATS")
+    }
+    private fun verifyToSubTabPage(teamPage: TeamPage) {
         teamPage.verifyTabIsSelected("TEAM STATS")
         teamPage.verifyHeaderIsPresented("STATS")
         teamPage.verifyHeaderIsPresented("(RANK)")
+    }
+    private fun navigateBackToMainPage(basePage: BasePage, mainPage: MainPage) {
         basePage.clickReturnButton()
         mainPage.verifyTeamIcon("TOR")
     }
